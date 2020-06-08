859031b00807cbc48eb204c658f6c278427e99ed
Jaime Sanders <jaimelyn517@gmail.com>
2020-06-08 15:10:13 -0700

    color routes written and tested

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -11,7 +11,6 @@ describe('createResponse', () => {
   });
 
   it('checks the /echo path', () => {
-
     return request(app)
       .post('/echo')
       .send('hello')
@@ -19,4 +18,29 @@ describe('createResponse', () => {
         expect(res.text).toEqual('hello');
       });
   });
+
+  it('checks the /red path', () => {
+    return request(app)
+      .get('/red')
+      .then(res => {
+        expect(res.text).toEqual('<h1>red</h1>');
+      });
+  });
+
+  it('checks the /green path', () => {
+    return request(app)
+      .get('/green')
+      .then(res => {
+        expect(res.text).toEqual('<h1>green</h1>');
+      });
+  });
+
+  it('checks the /blue path', () => {
+    return request(app)
+      .get('/blue')
+      .then(res => {
+        expect(res.text).toEqual('<h1>blue</h1>');
+      });
+  });
+
 });
